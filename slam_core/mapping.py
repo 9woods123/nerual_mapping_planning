3454107065588a9e51b571119c654d4444b4b36a
9woods123 <2726914442@qq.com>
2025-09-18 11:55:27 +0800

    add tracking , mapping , slam main , run slam at first frame

--- slam_core/mapping.py
+++ slam_core/mapping.py
@@ -25,31 +25,35 @@ class Mapper:
 
     def update_map(self, color, depth, camera_pose, is_frist_frame=False):
 
-        # === 生成射线数据 ===
-        rays_3d, rgb_values, depths = self.ray_casting.cast_rays(depth, color, camera_pose, depth.shape[0], depth.shape[1])
-        target_rgb = torch.tensor(rgb_values, dtype=torch.float32).to(self.device)
-
-        all_rays_points, all_rays_depths, _, all_rays_endpoint_depths = self.ray_casting.sample_points_along_ray(
-            ray_origin = camera_pose[:3, 3],  
-            rays_direction_list=rays_3d,
-            depths_list=depths
-        )
-
-        sampled_rays_points_tensor = torch.tensor(np.stack(all_rays_points), dtype=torch.float32).to(self.device)
-        sampled_rays_depths_tensor = torch.tensor(np.stack(all_rays_depths), dtype=torch.float32).to(self.device)
-        target_depth = torch.tensor(np.stack(all_rays_endpoint_depths), dtype=torch.float32).to(self.device)
-
         loss_val = 0.0
-        # === 多轮迭代优化 ===
-        
         iteration_number=0
 
         if is_frist_frame :
-            iteration_number=2*self.iters
+            iteration_number=5*self.iters
         else:
             iteration_number=self.iters
 
+
+
+        # === 多轮迭代优化 ===
+
         for i in range(iteration_number):
+
+            # === 生成射线数据 ===
+            rays_3d, rgb_values, depths = self.ray_casting.cast_rays(depth, color, camera_pose, depth.shape[0], depth.shape[1])
+            target_rgb = torch.tensor(rgb_values, dtype=torch.float32).to(self.device)
+
+            all_rays_points, all_rays_depths, _, all_rays_endpoint_depths = self.ray_casting.sample_points_along_ray(
+                ray_origin = camera_pose[:3, 3],  
+                rays_direction_list=rays_3d,
+                depths_list=depths
+            )
+
+            sampled_rays_points_tensor = torch.tensor(np.stack(all_rays_points), dtype=torch.float32).to(self.device)
+            sampled_rays_depths_tensor = torch.tensor(np.stack(all_rays_depths), dtype=torch.float32).to(self.device)
+            target_depth = torch.tensor(np.stack(all_rays_endpoint_depths), dtype=torch.float32).to(self.device)
+
+
             _, pred_sdfs, pred_colors = self.model(sampled_rays_points_tensor)
             rendered_color, rendered_depth = self.renderer.render(sampled_rays_depths_tensor, pred_sdfs, pred_colors)
 
